5226e0cd12bb158cd0cdfd53fb00e0a3a2220b1e
Jeonmanew <kalgomi9@gmail.com>
2025-04-08 02:35:03 +0900

    4강 애니메이션 전까지
    4강 무기교체 애니메이션 전까지 (무기교체가 안되요..)

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum Type { Ammo, Coin, Grenade, Heart, Weapon };
+    public Type type;
+    public int value;
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * 25 * Time.deltaTime);
+    }
+
+}
